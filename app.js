5b4104876c06b0775658f571c0e68d3129080a2d
YonatanHanan <yonatanhanann@gmail.com>
2016-12-17 20:54:56 +0200

    Generalizing the code
    Generalizing the code

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var fs = require('fs');
-var pg = require('pg');
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -8,18 +7,29 @@ var app = express();
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-app.post('/Chrome', function(req, res) {
+app.get('/Chrome/doesExitst/:md5_url', function(req, res) {
+    var rootpath = __dirname.replace(/\\/g, "/");
+
+    console.log(req.params.md5_url);
+    if (fs.existsSync("mhtml/" + req.params.md5_url + ".mhtml")) {
+        console.log("exists");
+        res.send({ state: true, location: rootpath + "/mhtml/" + req.params.md5_url + ".mhtml" });
+    } else {
+        res.send({ state: false });
+    }
+});
+
+app.post('/Chrome/save', function(req, res) {
 
     var base64Data = req.body.data.replace(/^data:;base64,/, "");
 
-
+    console.log(req.body.file_name);
     fs.writeFile("mhtml/" + req.body.file_name + ".mhtml", base64Data, 'base64', function(err) {
         if (err) {
             return console.log(err);
         }
+        var rootpath = __dirname.replace(/\\/g, "/");
 
-        var rootpath = __dirname.replace(/\\/g,"/") ;
-        
         console.log(req.body.file_name);
         res.send(rootpath);
     });
--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-filename = "";
 var goBack;
 
 function getAll() {
@@ -11,58 +10,52 @@ function clearAll() {
     chrome.storage.local.clear();
 }
 
-function clean(filename) {
-    var rg1 = /^(?=[\S])[^\\ \/ : * ? " < > | ]+$/;
-    filename = filename.replace(/[^\w\s]/gi, '');
-    return filename;
-}
-
 chrome.browserAction.onClicked.addListener(function(tab) {
     if (tab.url.indexOf(".mhtml") >= 0) {
-        // Open Live page
-        chrome.tabs.update(tab.id, { url: goBack.tab_url });
-        chrome.storage.local.remove(goBack.tab_md5);
-        goBack = null;
+        // Load live page and delete recorde
+        chrome.storage.local.get(null, function(items) {
+            var md5_url = tab.url.replace(/^.*[\\\/]/, '').replace(".mhtml", "");
+            if (items[md5_url]) {
+                chrome.storage.local.remove(md5_url);
+                goBack = "went";
+                chrome.tabs.update(tab.id, { url: items[md5_url].tab_url });
+            }
+        });
     } else {
         // Save the page
-        var tab_md5 = md5(tab.url);
+        var tab_md5 = md5(tab.url.split("#")[0]);
         chrome.pageCapture.saveAsMHTML({
             tabId: tab.id
         }, function(mhtmlData) {
             var reader = new window.FileReader();
             reader.readAsDataURL(mhtmlData);
             reader.onloadend = function() {
-                $.post("http://localhost:3000/Chrome", { "data": reader.result, "file_name": clean(tab.title) })
-                    .done(function(data) {
-                        chrome.tabs.update(tab.id, { url: data + "/mhtml/" + clean(tab.title) + ".mhtml" });
-                        var tab_path = data + "/mhtml/" + clean(tab.title) + ".mhtml";
+                $.post("http://localhost:3000/Chrome/save", { "data": reader.result, "file_name": tab_md5 }).done(function(data) {
+                    chrome.tabs.update(tab.id, { url: data + "/mhtml/" + tab_md5 + ".mhtml" });
+                    var tab_path = data + "/mhtml/" + tab_md5 + ".mhtml";
 
-                        chrome.storage.local.set({
-                            [tab_md5]: { "tab_md5": tab_md5, "tab_url": tab.url, "tab_lastupdated": +new Date(), "tab_path": tab_path, "tab_title": tab.title }
-                        });
+                    chrome.storage.local.set({
+                        [tab_md5]: { "tab_md5": tab_md5, "tab_url": tab.url, "tab_lastupdated": +new Date(), "tab_path": tab_path, "tab_title": tab.title }
                     });
+                });
             }
         });
-        goBack = "";
     }
 });
 
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    var tab_md5 = md5(changeInfo.url);
+    if (changeInfo.status == "loading" && changeInfo.url) {
 
-    if (typeof(changeInfo.url) == "string") {
-        if (changeInfo.status == "loading" || changeInfo.url.indexOf('/url?sa') < 0) {
-            chrome.storage.local.get(null, function(items) {
-                if (items[tab_md5]) {
-
-                    if (goBack != null) {
-                        chrome.tabs.update(tab.id, { url: items[tab_md5].tab_path });
-                        tab.url = items[tab_md5].tab_path;
-                    }
-                    goBack = items[tab_md5];
+        var md5_url = md5(changeInfo.url.split("#")[0]);
+        chrome.storage.local.get(null, function(items) {
+            if (items[md5_url]) {
+                if (goBack != "went") {
+                    chrome.tabs.update(tab.id, { url: items[md5_url].tab_path });
+                } else {
+                    goBack = "";
                 }
-            });
-        }
+            }
+        });
     }
 });
